018d6770a7c0b97f679f183d0bd1235f289655d9
Casperka <nikola.semradova@seznam.cz>
2021-11-29 13:16:46 +0100

    Added WordsApplication class and updated AndroidManifest

---
+++ app/src/main/java/com/example/android_room/WordsApplication.kt
@@ -0,0 +1,8 @@
+package com.example.android_room
+
+class WordsApplication : Application() {
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
